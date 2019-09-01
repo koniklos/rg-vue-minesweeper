cc875fd305049715375608d9b7ca9bbeb349c4fd
koniklos <hereticgr666@gmail.com>
2019-09-01 13:27:55 +0300

    added dist folder, changed build configuration in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,13 @@ module.exports = {
   configureWebpack: {
     optimization: {
       splitChunks: false
+    },
+    output: {
+      filename: "minesweeper.js"
     }
   },
   css: {
     extract: false
-  }
+  },
+  filenameHashing: false
 };
